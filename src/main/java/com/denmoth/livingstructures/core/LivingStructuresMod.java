77e6a3a0dc49a43c8b7e4f2dcd6daf70cd0921e3
Denmoth <denmoth8871top@gmail.com>
2025-10-26 14:34:51 +0200

    first commit

---
+++ src/main/java/com/denmoth/livingstructures/core/LivingStructuresMod.java
@@ -0,0 +1,12 @@
+package com.denmoth.livingstructures.core;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(LivingStructuresMod.MOD_ID)
+public class LivingStructuresMod {
+    public static final String MOD_ID = "living_structures";
+
+    public LivingStructuresMod() {
+        System.out.println("[Living Structures] Mod initialized successfully!");
+    }
+}
